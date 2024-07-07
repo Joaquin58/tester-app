f2ae4e14b9aa3c00490e1e2bf3bc5eb4973872d8
Joaquin Ramírez <91435597+Joaquin58@users.noreply.github.com>
2024-07-06 23:56:10 -0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -20,8 +20,6 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-conn.sync({ force: false }).then(() => {
   server.listen(process.env.PORT, () => {
     console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
   });
-});
